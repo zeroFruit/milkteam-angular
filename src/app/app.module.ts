cdf8beb97b7735222c1fcabea4a2c0652e3c4b20
hackurity <hackurity@hackurityui-MacBook-Pro.local>
2017-02-18 16:21:52 +0900

    random-video page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule }   from '@angular/router';
 
 // 유튜브모듈
 import { YoutubePlayerModule } from 'ng2-youtube-player';
@@ -11,24 +12,43 @@ import { NavComponent } from './nav/nav.component';
 import { BattleVideoComponent } from './battle-video/battle-video.component';
 import { BattleChatingComponent } from './battle-video/battle-chating/battle-chating.component';
 import { BattleChatMessageComponent } from './battle-video/battle-chating/battle-chat-message/battle-chat-message.component';
+import { RandomVideoComponent } from './random-video/random-video.component';
 import { VideoComponent } from './video/video.component';
+import { RandomInfoComponent } from "./random-video/random-info/random-info.component";
+import { RandomChattingComponent } from "./random-video/random-chatting/random-chatting.component";
 
 @NgModule({
-    imports: [
-        BrowserModule, YoutubePlayerModule
-    ],
-    declarations: [
-        AppComponent,
-        NavComponent,
-        BattleVideoComponent,
-        BattleChatingComponent,
-        BattleChatMessageComponent,
-        VideoComponent
-    ],
-    exports: [
-        AppComponent,
-    ],
-    bootstrap: [AppComponent]
+  imports     : [
+    BrowserModule,
+    YoutubePlayerModule,
+    RouterModule.forRoot([
+      {
+        path: 'main',
+        component: RandomVideoComponent
+      }, {
+        path: 'battle',
+        component: BattleChatingComponent
+      }, {
+        path: 'video',
+        component: VideoComponent
+      }
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    VideoComponent,
+    BattleVideoComponent,
+    BattleChatingComponent,
+    BattleChatMessageComponent,
+    RandomVideoComponent,
+    RandomInfoComponent,
+    RandomChattingComponent
+  ],
+  exports     : [
+    AppComponent,
+  ],
+  bootstrap   : [AppComponent]
 })
 export class AppModule {
 
---
+++ src/app/random-video/random-info/random-info.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by hackurity on 2017. 2. 16..
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'random-info',
+  templateUrl: 'random-info.component.html',
+  styleUrls: ['random-info.component.scss']
+})
+export class RandomInfoComponent {
+
+}
